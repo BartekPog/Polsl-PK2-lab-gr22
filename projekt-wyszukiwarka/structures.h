ce0ba51d6a566c5ff533f26e28b00f8b41f2d260
BartekPog <bartek.pogod@gmail.com>
2020-04-05 23:50:57 +0200

    Update structures, expand document structure

--- projekt-wyszukiwarka/structures.h
+++ projekt-wyszukiwarka/structures.h
@@ -3,19 +3,44 @@
 #ifndef STRUCTURES
 #define STRUCTURES
 
-/** One way linked list of char strings, inverted*/
+#include <stdio.h>
+
+/** One way linked list of char strings, inverted, stop-words replaced by "\0"*/
 struct stringLinkedListElem
 {
     char *value;                  ///< Word contained in a wordLinkedList
     struct wordLinkedList *pNext; ///< Pointer to the next element of linked list
 };
 
+/** Structure containing a single document*/
+struct document
+{
+    char header[300]; ///< header of the document
+    fpos_t startPos;  ///< Position of the beginning of the article in file
+    fpos_t endPos;    ///< Position of the ending of the article in file
+    int grams;        ///< Total number of grams
+};
+
+/** document one way linked list*/
+struct docLinkedListElem
+{
+    struct document *doc;            ///< Pointer to the stored document
+    struct docLinkedListElem *pNext; ///< Pointer to the next element of linked list
+};
+
+/** Array of pointers to documents ant its size*/
+struct docArray
+{
+    struct document **documentPointers; ///< Array of pointers to documents
+    int size;                           ///< Size of the docArray
+};
+
 /**
- * @brief Frees memory taken by the linked list. Also nullifies the list head, thus it has to be given as a pointer to head pointer.
+ * @brief Frees memory taken by the linked list. It does not nullify the pointer
  * 
- * @param pointerToHeadPointer pointer to the list head pointer 
+ * @param pointerToHeadPointer list head pointer 
  */
-void deleteStringLinkedList(struct stringLinkedListElem **pointerToHeadPointer);
+void deleteStringLinkedList(struct stringLinkedListElem *pHead);
 
 /**
  * @brief Adds element to the beginning (head) of the list
@@ -23,7 +48,7 @@ void deleteStringLinkedList(struct stringLinkedListElem **pointerToHeadPointer);
  * @param pHead head of the list 
  * @param value value to add to the list 
  */
-void addToListHead(struct stringLinkedListElem *pHead, char *value);
+void addToStringListHead(struct stringLinkedListElem *pHead, char *value);
 
 /**
  * @brief Counts how many elements are in the lis
@@ -32,5 +57,12 @@ void addToListHead(struct stringLinkedListElem *pHead, char *value);
  * @return int number of elements
  */
 int countStringLinkedListElements(struct stringLinkedListElem *pHead);
+
+/**
+ * @brief Frees the memory taken by the document array and all the documents stored in it
+ * 
+ * @param array pointer to the document array
+ */
+void deleteDocArray(struct docArray *array);
 
 #endif
